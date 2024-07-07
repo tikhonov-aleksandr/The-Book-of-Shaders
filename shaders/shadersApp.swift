c8b1a35c7d92ae14df15ecc4e3b542ea10c86cb7
tikhonov <tihonov.a56@wb.ru>
2024-07-07 10:25:30 +0200

    init

---
+++ shaders/shadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  shadersApp.swift
+//  shaders
+//
+//  Created by tikhonov on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct shadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
